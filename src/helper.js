a0f2b6c816cebf455c19b45fc797024e72cf7119
Pradeep Singh Rawat <pradeepsrawat029@gmail.com>
2022-11-01 10:49:46 +0530

    Add files via upload

---
+++ src/helper.js
@@ -0,0 +1,10 @@
+import HELP_2 from "./helper_2";
+
+export const JS_CONST = {
+  TEST: "TEST",
+  HELP_2: HELP_2.HELP_2_TEXT,
+};
+
+export function bar(url) {
+  return url;
+}
---
+++ src/helper_2.js
@@ -0,0 +1,8 @@
+import { JS_CONST } from "./helper";
+
+export const HELP_2 = {
+  HELP_2_TEXT: "HELP_2_TEXT",
+  HELP_2_CONNECT: JS_CONST.TEST,
+};
+
+export default HELP_2;
